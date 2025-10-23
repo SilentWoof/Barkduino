cd0ca4aea7365d328021fc66e47a7dac8877eb1a
SilentWoof <10962965+SilentWoof@useres.noreply.github.com>
2025-10-23 21:02:41 +0100

    Shorrtened and reordered some pose functions for clarity.

--- barkduino/poses/leg_primitives.h
+++ barkduino/poses/leg_primitives.h
@@ -16,54 +16,56 @@ void syncFrontLegs(int leftTarget, int rightTarget, int duration = 500);
 
 // 🧩 Primitive Leg Positions
 
-void rearLegsFoldedForward() {
-  Serial.println("Rear legs: folded forward");
-  syncRearLegs(0, 180);
-}
-
-void rearLegsMidwayToFoldedForward() {
-  Serial.println("Rear legs: midway to folded forward");
-  syncRearLegs(50, 120);  // halfway between straight and folded forward
-}
-
-void rearLegsStraight() {
-  Serial.println("Rear legs: straight");
-  syncRearLegs(100, 60);
-}
-
-void rearLegsMidwayToFoldedBack() {
-  Serial.println("Rear legs: midway to folded back");
-  syncRearLegs(140, 20);  // halfway between straight and folded back
-}
-
-void rearLegsFoldedBack() {
-  Serial.println("Rear legs: folded back");
-  syncRearLegs(180, 0);
-}
-
-void frontLegsMidwayToFoldedForward() {
-  Serial.println("Front legs: midway to folded forward");
-  syncFrontLegs(30, 145);  // halfway between straight and folded forward
-}
-
-void frontLegsFoldedForward() {
+// Front
+void FrontForward() {
   Serial.println("Front legs: folded forward");
   syncFrontLegs(0, 180);
 }
 
-void frontLegsStraight() {
+void FrontMidwayForward() {
+  Serial.println("Front legs: midway to folded forward");
+  syncFrontLegs(30, 145);
+}
+
+void FrontStraight() {
   Serial.println("Front legs: straight");
   syncFrontLegs(60, 110);
 }
 
-void frontLegsMidwayToFoldedBack() {
+void FrontMidwayBack() {
   Serial.println("Front legs: midway to folded back");
-  syncFrontLegs(110, 65);  // halfway between straight and folded back
+  syncFrontLegs(110, 65);
 }
 
-void frontLegsFoldedBack() {
+void FrontBack() {
   Serial.println("Front legs: folded back");
   syncFrontLegs(160, 20);
+}
+
+// Rear
+void RearForward() {
+  Serial.println("Rear legs: folded forward");
+  syncRearLegs(0, 180);
+}
+
+void RearMidwayForward() {
+  Serial.println("Rear legs: midway to folded forward");
+  syncRearLegs(50, 120);
+}
+
+void RearStraight() {
+  Serial.println("Rear legs: straight");
+  syncRearLegs(100, 60);
+}
+
+void RearMidwayBack() {
+  Serial.println("Rear legs: midway to folded back");
+  syncRearLegs(140, 20);
+}
+
+void RearBack() {
+  Serial.println("Rear legs: folded back");
+  syncRearLegs(180, 0);
 }
 
 #endif
