826841cb797370041f1bacc0ded6bd30ecbacf3a
SilentWoof <10962965+SilentWoof@users.noreply.github.com>
2025-10-23 23:43:32 +0100

    Added pounce trait.

---
+++ barkduino/traits/trait_pounce.h
@@ -0,0 +1,32 @@
+#ifndef TRAIT_POUNCE_H
+#define TRAIT_POUNCE_H
+
+#include <Arduino.h>
+#include "../poses/pose_composites.h"
+#include "../poses/leg_primitives.h"  // Needed for FrontStraight()
+#include "../configs/motion_config.h"  // Optional if timing constants are reused
+
+// 🐾 Trait: Pounce
+void trait_pounce() {
+  Serial.println("Trait: Pounce");
+
+  posePounce();        // Crouch with rear legs midway back, front legs forward
+  delay(750);          // Initial hold
+
+  FrontStraight();     // Extend front legs
+  delay(random(100, 3001));
+
+  posePounce();        // Return to pounce
+  delay(random(100, 3001));
+
+  FrontStraight();     // Extend again
+  delay(random(100, 3001));
+
+  posePounce();        // Return again
+  delay(random(100, 3001));
+
+  poseStand();         // Final stand
+  delay(random(100, 1001));
+}
+
+#endif
