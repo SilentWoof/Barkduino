da77c27e2d1e33ee5972d3fd69e53734ce56a10e
SilentWoof <10962965+SilentWoof@useres.noreply.github.com>
2025-11-02 19:30:35 +0000

    Updated trait registry to accomodate new traits.

--- barkduino/traits/trait_registry.h
+++ barkduino/traits/trait_registry.h
@@ -8,18 +8,24 @@
 #include "trait_sit.h"
 #include "trait_spread.h"
 #include "trait_wait.h"
+#include "trait_waggle_left.h"
+#include "trait_waggle_right.h"
+#include "trait_jump.h"
 
 // 🧠 Trait registry for modular behavior selection
 typedef void (*TraitFunction)();
 
 TraitFunction traitRegistry[] = {
-  trait_bow,         // 0
-  trait_point_left,  // 1
-  trait_point_right, // 2
-  trait_pounce,      // 3
-  trait_sit,         // 4
-  trait_spread,      // 5
-  trait_wait         // 6
+  trait_bow,           // 0
+  trait_point_left,    // 1
+  trait_point_right,   // 2
+  trait_pounce,        // 3
+  trait_sit,           // 4
+  trait_spread,        // 5
+  trait_wait,          // 6
+  trait_waggle_left,   // 7
+  trait_waggle_right,  // 8
+  trait_jump           // 9
 };
 
 #endif
